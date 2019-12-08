e96e89a60129e3a519ff75de793bd48fd4f0d4d3
harus2rock <izumi@katolab.nitech.ac.jp>
2019-12-08 17:19:44 +0900

    Create functions

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # slacktest
 My first slack bot
+
+[reference](https://qiita.com/sukesuke/items/1ac92251def87357fdf6)
---
+++ slackbot_settings.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+import os
+
+API_TOKEN = str(os.getenv('SLACKBOTTOKEN_TEST_HARU'))
+DEFAULT_REPLY = "わからん！"
+# Directory name which has plugin scripts
+PLUGINS = ['plugins']
